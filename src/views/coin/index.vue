f35e4d28f3bbe812f1a081555d181c03ffcc5d3d
VLADISLAW9 <duplinskij_v_d@mail.ru>
2025-03-22 17:16:07 +0700

    mini refactor

--- src/views/coin/index.vue
+++ src/views/coin/index.vue
@@ -5,6 +5,7 @@ import { useGetCoinQuery } from '@/utils/api/hooks';
 import { formatCurrency } from '@/utils/helpers';
 import Chart from 'primevue/chart';
 import type { ChartData } from 'chart.js';
+import { Button } from 'primevue';
 
 const route = useRoute();
 const getCoinQuery = useGetCoinQuery({ params: { uuid: `${route.params.uuid}` } });
@@ -23,8 +24,6 @@ const coinChartData = ref<ChartData>({
   ]
 });
 
-const generateTimeLabels = () => Array.from({ length: 23 }, (_, i) => `${i + 1}:00`);
-
 watch(
   () => getCoinQuery.data.value,
   (newData) => {
@@ -33,7 +32,7 @@ watch(
         .slice(0, -1)
         .map((item: string) => Number(item));
 
-      coinChartData.value.labels = generateTimeLabels();
+      coinChartData.value.labels = Array.from({ length: 23 }, (_, i) => `${i + 1}:00`);
       coinChartData.value.datasets[0].data = sparkline;
     }
   },
@@ -49,7 +48,7 @@ const coin = computed(() => getCoinQuery.data.value?.data.data.coin);
   </div>
   <div v-else-if="coin">
     <div class="flex gap-10">
-      <div class="flex w-[30%] flex-col gap-5">
+      <div class="flex w-[30%] flex-col gap-10">
         <div class="flex gap-3 items-center">
           <img class="w-7 h-7" :src="coin.iconUrl" alt="coin" />
           <div class="flex items-end gap-2">
@@ -79,6 +78,39 @@ const coin = computed(() => getCoinQuery.data.value?.data.data.coin);
             <p class="text-sm font-bold">
               {{ formatCurrency(Number(coin['24hVolume'])) }}
             </p>
+          </div>
+          <div class="border border-surface-800 rounded-2xl py-3 text-center">
+            <p class="text-sm text-gray-500">FDV</p>
+            <p class="text-sm font-bold">
+              {{ formatCurrency(Number(coin.fullyDilutedMarketCap)) }}
+            </p>
+          </div>
+        </div>
+        <div class="flex flex-col gap-5">
+          <div class="flex items-center justify-between">
+            <p class="font-semibold text-gray-500">Сайт</p>
+            <Button
+              rounded
+              severity="secondary"
+              size="small"
+              :href="coin.websiteUrl"
+              target="_blank"
+            >
+              <i class="pi pi-globe" />
+              <span class="text-sm">Перейти</span>
+            </Button>
+          </div>
+          <div class="flex items-center justify-between">
+            <p class="font-semibold text-gray-500">Изменения в цене за 24 часа</p>
+            <Button
+              rounded
+              variant="outlined"
+              :severity="Number(coin.change) > 0 ? 'danger' : 'success'"
+              size="small"
+            >
+              {{ Number(coin.change) > 0 ? '↓' : '↑' }}
+              <span class="text-sm"> {{ Math.abs(Number(coin.change)) }} </span>
+            </Button>
           </div>
         </div>
       </div>
